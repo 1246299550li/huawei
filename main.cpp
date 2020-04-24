cba3c2922ec8552ed26e0fc7086f76c5b998db70
1246299550li <1246299550@qq.com>
2020-04-24 14:20:59 +0800

    dfs

--- main.cpp
+++ main.cpp
@@ -9,38 +9,14 @@
 
 using namespace std;
 
-
-struct Path {
-    int len{};
-    unsigned int path[15]{};
-
-    explicit Path() = default;
-
-    Path(int length, const unsigned int *p) {
-        len = length;
-        for (int i = 0; i < len; i++)
-            path[i] = p[i];
-    }
-
-    bool operator<(const Path &rhs) const {
-        if (len == rhs.len) {
-            for (int i = 0; i < len; i++)
-                if (path[i] != rhs.path[i])
-                    return path[i] < rhs.path[i];
-        }
-        return len < rhs.len;
-    }
-
-};
-
 const int maxSize = 580005;
 
 int G[maxSize][100];
 int Ginv[maxSize][100];
 
-unordered_map<unsigned int, int> idHash; // card No -> id
-unsigned int ids[maxSize]; //id -> card No
-unsigned int inputs[maxSize];
+unordered_map<int, int> idHash; // card No -> id
+int ids[maxSize]; //id -> card No
+int inputs[maxSize];
 int inputNum;
 int nodeNum;
 
@@ -51,21 +27,50 @@ bool vis[maxSize] = {false};
 int isValid[maxSize];
 int isTail[maxSize];
 
-unsigned int cycle3[3 * 500000];
-unsigned int cycle4[4 * 500000];
-unsigned int cycle5[5 * 1000000];
-unsigned int cycle6[6 * 2000000];
-unsigned int cycle7[7 * 3000000];
-unsigned int *cycle[] = {cycle3, cycle4, cycle5, cycle6, cycle7};
+
+struct Cycle {
+    int cycle[7];
+
+    bool operator<(const Cycle &rhs) const {
+        for (int i = 0; i < 7; ++i) {
+            if (cycle[i] != rhs.cycle[i])
+                return cycle[i] < rhs.cycle[i];
+        }
+    }
+};
+
+Cycle cycle3[500000];
+Cycle cycle4[500000];
+Cycle cycle5[1000000];
+Cycle cycle6[2000000];
+Cycle cycle7[3000000];
+Cycle *cycle[] = {cycle3, cycle4, cycle5, cycle6, cycle7};
 int cycleNum[10] = {0};
 int out = 0;
-Path pathArr[3000000];
-int pathIdx = 0;
+
 vector<unordered_map<int, vector<int>>> pre(maxSize);
+
+int bucketSort(int *begin, const int *end) {
+    int *p = begin;
+    const int MAXID = 260000;
+    bool bucket[MAXID];
+    memset(bucket, 0, sizeof(bucket));
+    for (; p < end; ++p) {
+        bucket[*p] = true;
+    }
+    p = begin;
+    for (int i = 0; i < MAXID; i++) {
+        if (bucket[i]) {
+            *p = i;
+            ++p;
+        }
+    }
+    return p - begin;
+}
 
 void init(string &testFile) {
     FILE *file = fopen(testFile.c_str(), "r");
-    unsigned int u, v, value;
+    int u, v, value;
     while (fscanf(file, "%u,%u,%u", &u, &v, &value) != EOF) {
         inputs[inputNum++] = u;
         inputs[inputNum++] = v;
@@ -73,8 +78,7 @@ void init(string &testFile) {
 
     // card No -> id
     copy(begin(inputs), end(inputs), begin(ids));
-    sort(ids, ids + inputNum);
-    nodeNum = unique(ids, ids + inputNum) - ids;
+    nodeNum = bucketSort(ids, ids + inputNum);
     for (int i = 0; i < nodeNum; i++) {
         idHash[ids[i]] = i;
     }
@@ -123,31 +127,23 @@ void dfs_range2(int head, int cur, int len) {
     }
 }
 
-void dfs(int head, int cur, int len, unsigned int path[]) {
+void dfs(int head, int cur, int len, int path[]) {
     vis[cur] = true;
     path[len - 1] = cur;
     for (int i = 0; i < outDegrees[cur]; ++i) {
         int v = G[cur][i];
-        if (v < head || vis[v]) continue;
-        if (isValid[v] != head) continue;
+        if (v < head || vis[v] || isValid[v] != head) continue;
         if (isTail[v] == (-100 - head)) {
             for (auto &x :pre[head][v]) {
                 if (!vis[x]) {
                     path[len] = v;
                     path[len + 1] = x;
-                    unsigned int tmp[16];
-                    for (int j = 0; j < len + 2; j++)
-                        tmp[j] = ids[path[j]];
-                    pathArr[pathIdx++] = (Path(len + 2, tmp));
+                    for (int j = 0; j < len + 2; ++j) {
+                        cycle[len - 1][cycleNum[len + 2]++].cycle[j] = ids[path[j]];
+                    }
                     out++;
                 }
             }
-//            path[len] = v;
-//            for (int j = 0; j <= len; ++j) {
-//                cycle[len - 2][cycleNum[len + 1] * (len + 1) + j] = ids[path[j]];
-//                ++cycleNum[len + 1];
-//            }
-
         }
         if (len < 5) {
             dfs(head, v, len + 1, path);
@@ -157,7 +153,7 @@ void dfs(int head, int cur, int len, unsigned int path[]) {
 }
 
 void run() {
-    unsigned int path[16];
+    int path[16];
     for (int i = 0; i < nodeNum; ++i) {
         if (outDegrees[i] > 0 && inDegrees[i] > 0) {
             vis[i] = true;
@@ -168,54 +164,36 @@ void run() {
         }
     }
 
-    sort(pathArr, pathArr + pathIdx);
+
+//    sort(pathArr, pathArr + pathIdx);
 
 }
 
-//void output(string &outputFile) {
-//    int ansNum = 0;
-//    for (int k = 3; k <= 7; ++k) {
-//        ansNum += cycleNum[k];
-//    }
-//
-//    FILE *fp = fopen(outputFile.c_str(), "w");
-//    string tmp = to_string(ansNum) + "\n";
-//    const char *t = tmp.c_str();
-//    fwrite(t, strlen(t), 1, fp);
+void output(string &outputFile) {
+    int ansNum = 0;
+    for (int k = 3; k <= 7; ++k) {
+        ansNum += cycleNum[k];
+    }
+
+    FILE *fp = fopen(outputFile.c_str(), "w");
+    string tmp = to_string(ansNum) + "\n";
+    const char *t = tmp.c_str();
+    fwrite(t, strlen(t), 1, fp);
 //    for (int i = 3; i <= 7; ++i) {
-//        for (int j = 0; j < cycleNum[i]; j++) {
-//            string str = to_string(cycle[i - 3][i * j]);
+//        for (int j = 0; j < cycleNum[i]; ++j) {
+//            string str = to_string(cycle[i - 3][j].cycle[0]);
 //            for (int k = 0; k < i; ++k) {
-//                str += "," + to_string(cycle[i - 3][i * j + k]);
+//                str += "," + to_string(cycle[i - 3][j].cycle[k]);
 //            }
 //            str += "\n";
 //            const char *p = str.c_str();
 //            fwrite(p, strlen(p), 1, fp);
 //        }
 //    }
-//    fclose(fp);
-//}
-void output(string &outputFile) {
-
-    FILE *fp = fopen(outputFile.c_str(), "w");
-    string tmp = to_string(pathIdx) + "\n";
-    const char *t = tmp.c_str();
-    fwrite(t, strlen(t), 1, fp);
-    for (int j = 0; j < pathIdx; j++) {
-        auto x = pathArr[j];
-        auto path = x.path;
-        string str = to_string(path[0]);
-        for (int i = 1; i < x.len; i++)
-            str += "," + to_string(path[i]);
-        str += "\n";
-        const char *p = str.c_str();
-        fwrite(p, strlen(p), 1, fp);
-    }
     fclose(fp);
 }
 
 int main() {
-
     string testFile = "../data/test_data.txt";
     string outputFile = "../projects/student/result.txt";
     auto t = clock();
