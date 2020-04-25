1d7516faa0021d67b32a5bee64c9d1891396748a
1246299550li <1246299550@qq.com>
2020-04-25 11:48:10 +0800

    dfs

--- main.cpp
+++ main.cpp
@@ -13,14 +13,14 @@
 //#include <unistd.h>
 using namespace std;
 
-const int maxSize = 580005;
+const int maxSize = 280005;
 
-int G[maxSize][60];
-int Ginv[maxSize][60];
+int G[maxSize][600];
+int Ginv[maxSize][600];
 
-unordered_map<int, int> idHash; // card No -> id
-int ids[maxSize]; //id -> card No
-int inputs[maxSize];
+unordered_map<unsigned int, int> idHash; // card No -> id
+unsigned int ids[560000]; //id -> card No
+unsigned int inputs[560000];
 int inputNum;
 int nodeNum;
 
@@ -29,18 +29,11 @@ int outDegrees[maxSize] = {0};
 
 bool vis[maxSize] = {false};
 int isValid[maxSize];
-int isTail[maxSize];
+int isTail[maxSize] = {0};
 
 
 struct Cycle {
-    int cycle[7];
-
-    bool operator<(const Cycle &rhs) const {
-        for (int i = 0; i < 7; ++i) {
-            if (cycle[i] != rhs.cycle[i])
-                return cycle[i] < rhs.cycle[i];
-        }
-    }
+    unsigned int cycle[7];
 };
 
 Cycle cycle3[500000];
@@ -50,36 +43,36 @@ Cycle cycle6[2000000];
 Cycle cycle7[3000000];
 Cycle *cycle[] = {cycle3, cycle4, cycle5, cycle6, cycle7};
 int cycleNum[10] = {0};
-int out = 0;
+//int out = 0;
 
 vector<unordered_map<int, vector<int>>> pre(maxSize);
-//void next(const char* buf, int& len, unsigned int& u, unsigned int& v){
-//    const char* tmp = buf + len;
-//    u = 0;
-//    v = 0;
-//    while(*tmp && (*tmp != ',' && *tmp != 0x0d && *tmp != 0x0a && *tmp != '\n')){
-//        u = u * 10 + (*tmp - '0');
-//        ++tmp;
-//    }
-//    if (*tmp == ',') {
-//        ++tmp;
-//        while (*tmp != ',') {
-//            v = v * 10 + (*tmp - '0');
-//            ++tmp;
-//        }
-//        ++tmp;
-//        while (*tmp != 0x0d && *tmp != 0x0a && *tmp != '\n') {
-//            ++tmp;
-//        }
-//        len = tmp - buf + 1;
-//    } else{
-//        len = -1;
-//    }
-//}
+void next(const char* buf, int& len, unsigned int& u, unsigned int& v){
+    const char* tmp = buf + len;
+    u = 0;
+    v = 0;
+    while(*tmp && (*tmp != ',' && *tmp != 0x0d && *tmp != 0x0a && *tmp != '\n')){
+        u = u * 10 + (*tmp - '0');
+        ++tmp;
+    }
+    if (*tmp == ',') {
+        ++tmp;
+        while (*tmp != ',') {
+            v = v * 10 + (*tmp - '0');
+            ++tmp;
+        }
+        ++tmp;
+        while (*tmp != 0x0d && *tmp != 0x0a && *tmp != '\n') {
+            ++tmp;
+        }
+        len = tmp - buf + 1;
+    } else{
+        len = -1;
+    }
+}
 
-int bucketSort(int *begin, const int *end) {
-    int *p = begin;
-    const int MAXID = 260000;
+int bucketSort(unsigned int *begin, const unsigned int *end) {
+    unsigned int *p = begin;
+    const int MAXID = maxSize;
     bool bucket[MAXID];
     memset(bucket, 0, sizeof(bucket));
     for (; p < end; ++p) {
@@ -97,11 +90,12 @@ int bucketSort(int *begin, const int *end) {
 
 void init(string &testFile) {
     FILE *file = fopen(testFile.c_str(), "r");
-    int u, v, value;
+    unsigned int u, v, value;
     while (fscanf(file, "%u,%u,%u", &u, &v, &value) != EOF) {
         inputs[inputNum++] = u;
         inputs[inputNum++] = v;
     }
+
 //    unsigned int u, v;
 //    int fd = open(testFile.c_str(),O_RDONLY);
 //    int len = lseek(fd,0,SEEK_END);
@@ -119,7 +113,9 @@ void init(string &testFile) {
 
     // card No -> id
     copy(begin(inputs), end(inputs), begin(ids));
-    nodeNum = bucketSort(ids, ids + inputNum);
+    sort(ids, ids + inputNum);
+    nodeNum = unique(ids, ids + inputNum) - ids;
+//    nodeNum = bucketSort(ids, ids + inputNum);
     for (int i = 0; i < nodeNum; i++) {
         idHash[ids[i]] = i;
     }
@@ -128,13 +124,14 @@ void init(string &testFile) {
         G[s][outDegrees[s]++] = t;
         Ginv[t][inDegrees[t]++] = s;
     }
+    for (int j = 0; j < nodeNum; ++j) {
+        sort(G[j], G[j] + outDegrees[j]);
+        sort(Ginv[j], Ginv[j] + inDegrees[j]);
+    }
     memset(isValid, -1, sizeof(isValid));
-    memset(isTail, 0, sizeof(isTail));
-
 }
 
 void dfs_range1(int head, int cur, int len) {
-
     for (int i = 0; i < outDegrees[cur]; ++i) {
         int v = G[cur][i];
         if (v < head || vis[v])continue;
@@ -168,12 +165,12 @@ void dfs_range2(int head, int cur, int len) {
     }
 }
 
-void dfs(int head, int cur, int len, int path[]) {
+void dfs(int head, int cur, int len, unsigned int path[]) {
     vis[cur] = true;
     path[len - 1] = cur;
     for (int i = 0; i < outDegrees[cur]; ++i) {
         int v = G[cur][i];
-        if (v < head || vis[v] || isValid[v] != head) continue;
+        if (isValid[v] != head || vis[v])continue;
         if (isTail[v] == (-100 - head)) {
             for (auto &x :pre[head][v]) {
                 if (!vis[x]) {
@@ -183,7 +180,7 @@ void dfs(int head, int cur, int len, int path[]) {
                         cycle[len - 1][cycleNum[len + 2]].cycle[j] = ids[path[j]];
                     }
                     ++cycleNum[len + 2];
-                    ++out;
+//                    ++out;
                 }
             }
         }
@@ -195,7 +192,7 @@ void dfs(int head, int cur, int len, int path[]) {
 }
 
 void run() {
-    int path[16];
+    unsigned int path[16];
     for (int i = 0; i < nodeNum; ++i) {
         if (outDegrees[i] > 0 && inDegrees[i] > 0) {
             vis[i] = true;
@@ -205,13 +202,6 @@ void run() {
             dfs(i, i, 1, path);
         }
     }
-
-    sort(cycle3, cycle3 + cycleNum[3]);
-    sort(cycle4, cycle4 + cycleNum[4]);
-    sort(cycle5, cycle5 + cycleNum[5]);
-    sort(cycle6, cycle6 + cycleNum[6]);
-    sort(cycle7, cycle7 + cycleNum[7]);
-
 }
 
 void output(string &outputFile) {
@@ -219,7 +209,6 @@ void output(string &outputFile) {
     for (int k = 3; k <= 7; ++k) {
         ansNum += cycleNum[k];
     }
-
     FILE *fp = fopen(outputFile.c_str(), "w");
     string tmp = to_string(ansNum) + "\n";
     const char *t = tmp.c_str();
@@ -242,6 +231,7 @@ void output(string &outputFile) {
 
 int main() {
     string testFile = "../data/test_data.txt";
+
     string outputFile = "../projects/student/result.txt";
     auto t = clock();
     init(testFile);
@@ -252,7 +242,7 @@ int main() {
 //    t = clock();
     output(outputFile);
     cout << "time:" << double(clock() - t) / CLOCKS_PER_SEC << "s" << endl;
-    cout << out << endl;
+//    cout << out << endl;
     return 0;
 }
 
